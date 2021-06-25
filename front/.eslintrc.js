62e613e485f5aa0100d5a57dc66964bb7db44df1
gmldnjs26 <imleesky265@gmail.com>
2021-06-26 03:07:19 +0900

    fix eslint

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -7,14 +7,10 @@ module.exports = {
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+  // add your custom rules here
+  rules: {},
 };

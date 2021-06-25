d855cc23aab115fcbc0d4f51ababe28602413449
gmldnjs26 <imleesky265@gmail.com>
2021-06-26 03:07:38 +0900

    layout

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,19 +1,73 @@
+
 <template>
-  <div class="bg-blue-500" id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <div class="bg-blue-500 text-2xl">asd</div>
+  <div class="flex h-full min-h-[1123px] bg-gray-300">
+    <transition name="fade">
+      <div v-show="isOpen || curWidth > 940" class="w-60">
+        <SideBar @toggleBtnClick="toggleBtnClick" />
+      </div>
+    </transition>
+    <div
+      class="flex flex-col flex-1 mx-auto min-w-[1162px] max-w-[1162px]"
+      :class="
+        isOpen ? 'opacity-30 absolute left:0 top:0 pointer-events-none' : ''
+      "
+    >
+      <div class="h-20">
+        <Header @toggleBtnClick="toggleBtnClick" />
+      </div>
+      <transition name="page">
+        <div class="flex-1">
+          <div class="bg-white" id="app">
+            <div>이부분의 내용이</div>
+            <div>주로 바뀌면서 페이지를 이동하는걸로</div>
+            <div>하는겁니다</div>
+            <img alt="Vue logo" src="./assets/logo.png" />
+            <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+          </div>
+        </div>
+      </transition>
+      <div class="h-16">
+        <Footer />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import SideBar from "@/components/organisms/SideBar.vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import Header from "@/components/organisms/Header.vue";
+import Footer from "@/components/organisms/Footer.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
+    Header,
+    Footer,
     HelloWorld,
+    SideBar,
+  },
+  data() {
+    return {
+      isOpen: false,
+      curWidth: window.innerWidth,
+    };
+  },
+  mounted() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+
+  methods: {
+    handleResize() {
+      this.curWidth = window.innerWidth;
+    },
+    toggleBtnClick() {
+      this.isOpen = !this.isOpen;
+    },
   },
 });
 </script>
@@ -26,5 +80,22 @@ export default Vue.extend({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: margin 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  margin-left: -230px;
+}
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
 }
 </style>
---
+++ front/src/components/organisms/SideBar.vue
@@ -0,0 +1,83 @@
+<template>
+  <aside
+    class="
+      flex
+      fixed
+      top-0
+      z-10
+      flex-col
+      w-60
+      min-h-1123
+      text-xs
+      font-normal
+      bg-gray-200
+    "
+  >
+    <!-- Title -->
+    <div class="flex relative pc:flex-col justify-center items-center h-20">
+      <div>Company Name</div>
+      <button
+        class="absolute top-4 right-4 text-white"
+        @click="$emit('toggleBtnClick')"
+      >
+        X
+      </button>
+    </div>
+    <!-- User Profile -->
+    <div class="flex flex-col justify-center items-center h-36 bg-gray-700">
+      <img class="rounded-full" src=" http://picsum.photos/60" />
+      <p class="mt-2 text-xs font-normal text-white">hee won</p>
+    </div>
+
+    <!-- Navigation -->
+    <nav class="pt-3">
+      <div v-for="page in pages" :key="page.id">
+        <div class="h-20 text-black flex items-center justify-center text-lg">
+          {{ page.name }}
+        </div>
+      </div>
+    </nav>
+  </aside>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "SideBar",
+
+  data() {
+    return {
+      pages: [
+        {
+          name: "제품목록",
+          id: 1,
+        },
+        {
+          name: "입고/출고",
+          id: 2,
+        },
+        {
+          name: "히스토리",
+          id: 3,
+        },
+        {
+          name: "대시보드",
+          id: 4,
+        },
+        {
+          name: "설정",
+          id: 5,
+        },
+        {
+          name: "마이페이지",
+          id: 6,
+        },
+        {
+          name: "문의",
+          id: 7,
+        },
+      ],
+    };
+  },
+});
+</script>
